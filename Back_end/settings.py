b67576f59ed6645de651f2db3c1fd6d3b6a454b2
yasmim.silva <yasmim.silva@fpf.br>
2025-07-30 15:16:54 -0400

    correçao settings

--- Back_end/settings.py
+++ Back_end/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core.apps.CoreConfig',
     'rest_framework',
     'core'
 ]
