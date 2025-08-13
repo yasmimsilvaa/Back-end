540cbc9f0d38378ee2f7d3f6a23fb820ef81c877
yasmim.silva <yasmim.silva@fpf.br>
2025-08-13 14:04:07 -0400

    paginacao e filtros

--- todolist_backend/settings.py
+++ todolist_backend/settings.py
@@ -38,6 +38,13 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ]
 }
 
 
